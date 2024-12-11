8732adcae6032e8f2033d34bd40571c74952579f
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-12-11 20:38:11 +0100

    first

---
+++ src/index.mjs
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Running on Port ${PORT}`);
+});
+
+//localhost:3000
+//localhost:3000/users
+//localhost:3000/products
